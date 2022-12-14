485cf1aac7368d154b0ed5019b99b8e31d4281b4
Keitiek <keiti.ekart@gmail.com>
2022-12-14 13:54:38 +0200

    rattaste liikumise kood

--- packages/my_package/src/my_publisher_node.py
+++ packages/my_package/src/my_publisher_node.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python3
-
 import os
 import rospy
+import smbus2
 from duckietown.dtros import DTROS, NodeType
 from std_msgs.msg import String
+from smbus2 import SMBus
+from duckietown_msgs.msg import WheelsCmdStamped
+
+speed = WheelsCmdStamped()
 
 class MyPublisherNode(DTROS):
-
+    
     def __init__(self, node_name):
         # initialize the DTROS parent class
         super(MyPublisherNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
         # construct publisher
-        self.pub = rospy.Publisher('chatter', String, queue_size=10)
-
+        self.pub = rospy.Publisher('/xgolder/wheels_driver_node/wheels_cmd', WheelsCmdStamped, queue_size=10)
+        
+    def on_shutdown(self):
+        speed.vel_left = 0
+        speed.vel_right = 0
+        self.pub.publish(speed)
+        rospy.on_shutdown()
+        
     def run(self):
         # publish message every 1 second
-        rate = rospy.Rate(1) # 1Hz
+        rate = rospy.Rate(20) # 1Hz
         while not rospy.is_shutdown():
-            message = "Hello from %s" % os.environ['VEHICLE_NAME']
-            rospy.loginfo("Publishing message: '%s'" % message)
-            self.pub.publish(message)
+            bus = SMBus(1)
+            speed.vel_left = 1
+            speed.vel_right = 1
+            read = bus.read_byte_data(62,17)
+            self.pub.publish(speed)
             rate.sleep()
-
+            bus.close()
+            print(read)
+            
 if __name__ == '__main__':
     # create the node
     node = MyPublisherNode(node_name='my_publisher_node')
@@ -29,3 +43,14 @@ if __name__ == '__main__':
     node.run()
     # keep spinning
     rospy.spin()
+
+
+
+
+
+
+
+
+
+
+
